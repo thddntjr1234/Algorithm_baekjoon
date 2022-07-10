c24c403a73dd5487b899629f56d5ae58d0e07b5c
thddntjr1234 <thddntjr112233@gmail.com>
2022-07-10 19:17:03 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 108 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,23 +1,20 @@
-num = 10000
-arr = [False, False] + [True] * (num-1)
+import sys
+arr = [False, False] + [True] * (9999)
 
-for i in range(2, num+1):
+for i in range(2, 101):
     if arr[i]:
-        for j in range(i*2, num+1, i):
+        for j in range(i*2, 10001, i):
             arr[j] = False
             
 
 cycle = int(input())
-count = 0
-while count < cycle:
-    count += 1
-    n = int(input())
+for _ in range(cycle):
+    n = int(sys.stdin.readline())
     a, b = n//2, n//2
     
     for i in range(n//2):
         if arr[a] and arr[b]:
             print(a, b)
             break
-        else:
-            a -= 1
-            b += 1
+        a -= 1
+        b += 1
