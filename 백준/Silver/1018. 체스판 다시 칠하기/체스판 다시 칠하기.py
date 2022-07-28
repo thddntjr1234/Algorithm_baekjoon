2d852fd7773bafdaee879ffb9a646252c1a18f4c
thddntjr1234 <thddntjr112233@gmail.com>
2022-07-28 19:45:20 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 100 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -6,16 +6,12 @@ def Repainting(M, N, board):
         for j in range(M-7):
             count_W = 0
             count_B = 0
-            kk = 0
             for k in range(i, i+8):    
-                ll = 0
                 for l in range(j, j+8):
-                    if board[k][l] != goalPaint_W[kk][ll]:
+                    if board[k][l] != goalPaint_W[k-i][l-j]:
                         count_W += 1
-                    elif board[k][l] != goalPaint_B[kk][ll]:
+                    elif board[k][l] != goalPaint_B[k-i][l-j]:
                         count_B += 1
-                    ll += 1
-                kk += 1
             countList.append(count_W)
             countList.append(count_B)
     return min(countList)
