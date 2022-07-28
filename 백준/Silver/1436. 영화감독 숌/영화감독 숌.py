d3deed35dfe22ec91876ec0c00d02b31ff9bb4c2
thddntjr1234 <thddntjr112233@gmail.com>
2022-07-28 22:31:33 +0900

    [Silver V] Title: 영화감독 숌, Time: 640 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,19 +1,34 @@
-def ShomNumber(N):
-    n = 0
-    shomCount = 0
-    while True:
-        sixCount = 0
-        for i in str(n):
-            if i == '6':
-                sixCount += 1
-            else:
-                sixCount = 0
-            if sixCount >= 3:
-                shomCount += 1
-                break
-        if shomCount == N:
-            return n
-        n += 1
+# def ShomNumber(N):
+#     n = 0
+#     shomCount = 0
+#     while True:
+#         sixCount = 0
+#         for i in str(n):
+#             if i == '6':
+#                 sixCount += 1
+#             else:
+#                 sixCount = 0
+#             if sixCount >= 3:
+#                 shomCount += 1
+#                 break
+#         if shomCount == N:
+#             return n
+#         n += 1
 
+# N = int(input())
+# print(ShomNumber(N))
+# --> 1672ms, 30840kb, 395byte non-pythonic code
+
+def ShomNumber(N):
+    n = 1
+    count = 0
+    while True:    
+        if '666' in str(n):
+            count += 1
+            if count == N:
+                break
+        n += 1
+    return n
 N = int(input())
 print(ShomNumber(N))
+## --> pythonic code
