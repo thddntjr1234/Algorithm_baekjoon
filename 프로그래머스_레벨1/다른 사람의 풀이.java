2f21512e6761eebb5dce088ea7cfa06f6dec6f22
thddntjr1234 <thddntjr112233@gmail.com>
2022-08-27 02:02:25 +0900

    프로그래머스

---
+++ 프로그래머스_레벨1/다른 사람의 풀이.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int[] arr) {
+
+        int tmp = arr[0];
+        List<Integer> tmpResult = new ArrayList<>();
+        tmpResult.add(tmp);
+
+        for (int i=1; i<arr.length; i++) {
+            if (arr[i] != tmp) tmpResult.add(arr[i]);
+            tmp = arr[i];
+        }
+
+        int[] answer = new int[tmpResult.size()];
+        for (int i=0; i<tmpResult.size(); i++) {
+            answer[i] = tmpResult.get(i);
+        }
+
+
+        return answer;
+    }
+}
