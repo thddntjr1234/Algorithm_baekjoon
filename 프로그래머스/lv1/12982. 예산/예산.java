0edcfb178f3633bad9b685ebcca519811a4ed5f6
thddntjr1234 <thddntjr112233@gmail.com>
2022-09-05 20:33:39 +0900

    [level 1] Title: 예산, Time: 0.37 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        
+        for(int cost: d) {
+            if(budget >= cost) {
+                budget -= cost;
+                answer += 1;
+            }
+        }
+        return answer;
+    }
+}
