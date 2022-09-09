de02358eeacfd1abf934a9579a0c023bec364c44
thddntjr1234 <thddntjr112233@gmail.com>
2022-09-09 17:45:59 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 11.29 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -5,9 +5,8 @@ class Solution {
         int[] list = Arrays.stream(s.split(" ")).mapToInt(Integer::parseInt).toArray();
         int max = Arrays.stream(list).max().getAsInt();
         int min = Arrays.stream(list).min().getAsInt();
-        StringBuilder sb = new StringBuilder();
-        sb.append(min).append(" ").append(max);
-        answer = sb.toString();
+        
+        answer = min + " " + max;
         return answer;
     }
 }
