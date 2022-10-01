2984b2e37c0982665fe8c3c17ea5b0bafd551fa1
thddntjr1234 <thddntjr112233@gmail.com>
2022-10-01 23:56:39 +0900

    [Silver III] Title: 터렛, Time: 136 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.java
+++ 백준/Silver/1002. 터렛/터렛.java
@@ -1,70 +1,43 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
- 
+import java.util.*;
+import java.io.*;
+
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
- 
-		int T = Integer.parseInt(br.readLine());
- 
-		while (T-- > 0) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
- 
-			int x1 = Integer.parseInt(st.nextToken());
-			int y1 = Integer.parseInt(st.nextToken());
-			int r1 = Integer.parseInt(st.nextToken());
- 
-			int x2 = Integer.parseInt(st.nextToken());
-			int y2 = Integer.parseInt(st.nextToken());
-			int r2 = Integer.parseInt(st.nextToken());
-			
-			System.out.println(tangent_point(x1, y1, r1, x2, y2, r2));
-		}
- 
-	}
- 
- 
-	// 접점 개수 구하는 함수
-	public static int tangent_point(int x1, int y1, int r1, int x2, int y2, int r2) {
-    
-		int distance_pow = (int)(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));	// 중점간 거리 distance의 제곱 
- 
- 
-		// case 1 : 중점이 같으면서 반지름도 같을 경우
-		if(x1 == x2 && y1 == y2 && r1 == r2) {
-			return -1;
-		}
-		
-		// case 2-1 : 두 원의 반지름 합보다 중점간 거리가 더 길 때 
-		else if(distance_pow > Math.pow(r1 + r2, 2)) {
-			return 0;
-		}
- 
-		// case 2-2 : 원 안에 원이 있으나 내접하지 않을 때 
-		else if(distance_pow < Math.pow(r2 - r1, 2)) {
-			return 0;
-		}
-		
-		// case 3-1 : 내접할 때 
-		else if(distance_pow == Math.pow(r2 - r1, 2)) {
-			return 1;
-		}
-        
-		
-		// case 3-2 : 외접할 때 
-		else if(distance_pow == Math.pow(r1 + r2, 2)) {
-			return 1;
-		}
-		
-		else {
-			return 2;
-		}
-		
-	}
- 
+    public static void main(String[] args) throws IOException {
+        int x1 = 0, x2 = 0, y1 = 0, y2 = 0, r1 = 0, r2 = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int k = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<k; i++) {
+            String s = br.readLine();
+            StringTokenizer st = new StringTokenizer(s);
+            x1 = Integer.parseInt(st.nextToken());
+            y1 = Integer.parseInt(st.nextToken());
+            r1 = Integer.parseInt(st.nextToken());
+            x2 = Integer.parseInt(st.nextToken());
+            y2 = Integer.parseInt(st.nextToken());
+            r2 = Integer.parseInt(st.nextToken());
+
+            int distance = (int)(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+            // 원의 사이즈도 같고, 좌표도 같은 경우 
+            if(x1 == x2 && y1 == y2 && r1 == r2) {
+                System.out.println(-1); 
+            }
+            // 두 원의 반지름 합보다 중심 좌표 차이가 더 길 때(원이 서로 닿지 않고 떨어진 경우)
+            // 원 안에 원이 있지만 내접하지 않을 때(큰 원의 반지름이 큰 원과 작은 원의 좌표차 + 작은 원의 반지름값보다 클 때)
+            else if(distance > Math.pow(r1 + r2, 2) || distance < Math.pow(r2 - r1, 2)) {
+                System.out.println(0);
+            }
+
+            // 두 원이 안에서 내접하거나(원의 반지름 차이만큼 중심 좌표가 차이난다면 원의 크기는 다르나 한 점에서 만남)
+            // 외부에서 서로 접하는 경우(각 원의 반지름 합과 중심 좌표차이의 합이 같으면 원은 한 점에서 만나게 됨)
+            else if(distance == Math.pow(r2 - r1, 2) || distance == Math.pow(r1 + r2, 2)) {
+                System.out.println(1);
+            }
+            
+            // 이외 케이스에서는 원이 두 점에서 만나게 된다.
+            else System.out.println(2);
+        }
+    }
 }
