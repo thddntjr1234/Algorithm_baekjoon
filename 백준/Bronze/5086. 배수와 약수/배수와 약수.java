d58af793d1bb2ea59f71697746871b394fb275a3
thddntjr1234 <thddntjr112233@gmail.com>
2022-10-02 18:13:09 +0900

    [Bronze III] Title: 배수와 약수, Time: 200 ms, Memory: 17520 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            if(A == 0 && B == 0) break;
+            
+            if(B % A == 0) System.out.println("factor");
+            else if(A % B == 0) System.out.println("multiple");
+            else System.out.println("neither");
+        }
+    }
+}
