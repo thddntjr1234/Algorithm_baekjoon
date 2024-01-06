b5fac76aa2a1ae43e11a99c89918d46afc54ea0a
Wooseok Song <thddntjr112233@gmail.com>
2024-01-06 11:27:00 +0900

    [Silver IV] Title: 괄호, Time: 128 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,27 +1,57 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-
+    public static void main(String[] args) throws IOException {
         /*
-         * ( 일땐 cnt++ )일땐 cnt--이고
-         * 올바른 VPS이려면 (로 시작해서 )로 닫혀야 하므로 cnt 결과가 0이면서 중간에 cnt가 음수로 되지 않아야 한다.
+         * 스택 자료구조를 사용하면 될 것으로 보이는데
+         * 괄호 쌍에 대해 '('괄호는 push, ')'괄호는 pop을 수행하면 될 것 같다.
          */
-        for (int i=0; i<T; i++) {
-            String str = br.readLine();
-            int cnt = 0;
-            for (int j=0; j<str.length(); j++) {
-                if(str.charAt(j) == '(') cnt++;
-                else cnt--;
 
-                if(cnt < 0) break;
+         // 1. 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int T = Integer.parseInt(br.readLine());
+        for (int cnt = 0; cnt<T; cnt++) {
+            String input = br.readLine();
+            char[] arr = input.toCharArray();
+
+            // 2. 검증
+            if (isVPS(arr)) {
+                sb.append("YES").append("\n");
+            } else {
+                sb.append("NO").append("\n");
             }
-            
-            if(cnt == 0) System.out.println("YES");
-            else System.out.println("NO");
+        }
+
+        // 3. 결과
+        System.out.println(sb);
+    }
+
+    static boolean isVPS(char[] arr) {
+
+        Stack<Character> st = new Stack<>();
+
+        for (int i=0; i<arr.length; i++) {
+            if (arr[i] == '(') {
+                st.push(arr[i]);
+            } else if (arr[i] == ')' && !st.isEmpty()) {
+                st.pop();
+            } else {
+                return false;
+            }
+        }
+
+        if (st.isEmpty()) {
+            return true;
+        } else { 
+            return false;
         }
     }
 }
